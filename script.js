fb9d3e6d49c0d12fdad8834caffdb18737c285c0
Pastafredda <justspotify09@gmail.com>
2023-05-11 00:25:07 +0200

    fine

--- script.js
+++ script.js
@@ -8,11 +8,61 @@ play.addEventListener('click',
     }
 )
 
-// Creiamo 100 quadratini da aggiungere a grid
-for(let i= 0; i < 100; i++){
+for (let i = 1; i < 100 + 1; i++){
     
-    // Aggiungiamo i quadratini a grid
-    const square =`<div class="square"></div>`;
-    grid.innerHTML += square;
+    const newSquare = createGridSquare("div", "square");
+  
+    newSquare.addEventListener("click",
+        function () {
+            newSquare.classList.add("square-green");
+            const span = document.createElement("span")
+            span.append(i);
+            newSquare.append(span);
+            console.log(i);
+        }
+    )
+
+    grid.append(newSquare);
+
+
+}
+
+
+function createGridSquare(tagType, classToAdd) {
+    const newElement = document.createElement(tagType);
+    newElement.classList.add(classToAdd);
+    return newElement;
     
 }
+
+
+
+
+
+
+
+// // Creiamo 100 quadratini da aggiungere a grid
+// for(let i= 0; i < 100; i++){
+
+//     // Aggiungiamo i quadratini a grid
+//     const squareGrid = square();
+    
+//     squareGrid.addEventListener("click",
+//         function () {
+//             squareGrid.classList.add("square-green");
+//         }
+//     )
+
+//     grid.innerHTML += squareGrid;
+
+   
+// }
+
+// // Creiamo una funzione per inserire la classe square nella griglia
+// function square(){
+//     const newSquare =`<div class="square"></div>`;
+//     return newSquare
+// }
+
+
+
